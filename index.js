262962a8780219d2b0b9fd0a99de147073e3b966
Farha Lina <farlinaha@gmail.com>
2021-04-19 22:26:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,31 +10,3 @@ function divide(){
 function add(a + b){
   return (a + b);
 }
-
-var number = 10
-
-function add5() {
-  number += 5;
-}
-
-function divideBy3() {
-  number /= 3;
-}
-
-divideBy3();
-
-console.log(number);
-
-add5();
-
-console.log(number);
-
-number = 10;
-
-add5();
-
-console.log(number);
-
-divideBy3();
-
-console.log(number);
